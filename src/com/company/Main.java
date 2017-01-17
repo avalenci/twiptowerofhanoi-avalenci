26364aaa1f1d5f7f516cfa65ab1f3e0a462400a8
avalenci <ansmeew@gmail.com>
2017-01-17 14:28:34 -0500

    Second commit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("Tower of Hanoi Game.\nThis program will solve this puzzle for you.\n" +
+                "Please input number of disks to solve for:");
+        Scanner input = new Scanner(System.in);
+        while (!(input.hasNextInt())) {
+            System.out.println("Try again.");
+            String x = input.next();
+        }
+        int x = input.nextInt();
     }
 }
